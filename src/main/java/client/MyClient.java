11ecaf440951b68404c1f1c84328db7659ae6aff
Jacob <jacobstanbrough@gmail.com>
2024-01-06 13:46:02 -0600

    added innerworkings to client, still working on listening method

--- src/main/java/client/MyClient.java
+++ src/main/java/client/MyClient.java
@@ -1,4 +1,38 @@
 package client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.Socket;
+
 public class MyClient {
+    private Socket client;
+    private BufferedReader in;
+
+    public MyClient(int portNumber) {
+        start(portNumber);
+    }
+
+    public void start(int portNumber) {
+        if (!initClient(portNumber)) {
+            try {
+                throw new IOException("Failed to init client");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        // continue here
+        startListening();
+    }
+
+    public boolean initClient(int portNumber) {
+        try {
+            client = new Socket("server", portNumber);
+            System.out.println("Client created");
+            return true;
+        } catch (IOException e) {
+            System.out.println("Client not created");
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
